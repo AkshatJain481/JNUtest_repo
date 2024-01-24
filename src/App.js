819d7f8d0cb2f002fae4285d884ef7ba7a3fddba
Akshat <akshatjain481@gmail.com>
2024-01-24 23:16:40 +0530

    fixed navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Navbar from './Navbar';
 import './App.css';
-
+import Dashboard from './Dashboard';
 function App() {
   return (
     <>
---
+++ src/Dashboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "./Navbar";
+function Dashboard(){
+    return(
+        <>
+        <Navbar/> 
+        {/* yhe tera navbar wala component hai jah pr bhi yhe same to same likhega wah pr navbar aa jayega tera */}
+        
+        </>
+    )
+}
+export default Dashboard;
