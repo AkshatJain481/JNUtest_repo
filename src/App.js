72b381a097b5d8ce90223efddaf19f26889f7bab
Akshat <akshatjain481@gmail.com>
2024-01-24 23:19:35 +0530

    added file for dashboard

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import Dashboard from './Dashboard';
 function App() {
   return (
     <>
-   <Navbar/>
+   <Dashboard/>
+   {/* yha likhna hai dekhliyo navbar ke neeche na aaye agar navbar ke neeche aa rah ho aur koi changes navbar mein krne pde toh navbar wali file mein changes krlio woh dashboard pr apne aap dikh jayenge */}
     </>
   );
 }
