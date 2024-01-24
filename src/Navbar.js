a85cd99f56fa46bd9c3abd1fc4ddd044798de274
Akshat <akshatjain481@gmail.com>
2024-01-24 21:21:31 +0530

    repo

---
+++ src/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+function Navbar(){
+    
+    return(
+        <>
+        yha likde apna html yatinder
+        </>
+    )
+}
+
+export default Navbar;
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+        
+      },
+      colors: {
+        cyan: '#00ffff',
+        orange: '#ef6b48',
+        new: '#dd501d',
+        new1: '#FFA94D',
+      },
+      width:{
+        '400' :'400px',
+        '500' :'500px',
+      }
+    },
+  },
+  plugins: [],
+};
